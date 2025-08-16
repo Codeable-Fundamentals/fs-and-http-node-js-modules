9fd3cd1b5da0f2d9ea24dfcc475ce5336c27ea54
Grecia Delgado <greciadm2596@gmail.com>
2025-08-15 18:57:47 -0600

    update code

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import http from "node:http";
+import fs from "node:fs";
+
+const PORT = 8000;
+const server = http.createServer((req, res) => {
+  if (req.url === "/index.html") {
+    fs.readFile("public/index.html", "utf-8", (err, content) => {
+      if (err) {
+        console.log(err);
+        res.writeHead(500);
+        res.end("internal server error!");
+      }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(content);
+    });
+  } else {
+    res.writeHead(404);
+    res.end(
+      JSON.stringify({
+        message: "Wrong path url",
+      })
+    );
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
