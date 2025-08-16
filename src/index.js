f49a0c9ff624110d683796ef7653f948b1850e4b
Grecia Delgado <greciadm2596@gmail.com>
2025-08-15 19:32:50 -0600

    add example of a server for many types of file paths

--- src/index.js
+++ src/index.js
@@ -1,26 +1,24 @@
 import http from "node:http";
 import fs from "node:fs";
 
+const publicDirectory = "./public";
+
 const PORT = 8000;
 const server = http.createServer((req, res) => {
-  if (req.url === "/index.html") {
-    fs.readFile("public/index.html", "utf-8", (err, content) => {
-      if (err) {
-        console.log(err);
-        res.writeHead(500);
-        res.end("internal server error!");
-      }
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(content);
-    });
-  } else {
-    res.writeHead(404);
-    res.end(
-      JSON.stringify({
-        message: "Wrong path url",
-      })
-    );
-  }
+  let filepath =
+    req.url === "/"
+      ? `${publicDirectory}/index.html`
+      : `${publicDirectory}${req.url}`;
+
+  fs.readFile(filepath, "utf-8", (err, content) => {
+    if (err) {
+      console.log(err);
+      res.writeHead(500);
+      res.end("internal server error!");
+    }
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(content);
+  });
 });
 
 server.listen(PORT, () => {
